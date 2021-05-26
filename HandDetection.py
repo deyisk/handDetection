81eab590a6a459c5f9ffc7c43b425ad0f35bac14
deyisk <deyiskarakas@hotmail.com>
2021-05-26 23:39:12 +0300

    handDetection

--- HandDetection.py
+++ HandDetection.py
@@ -14,17 +14,16 @@ cTime = 0
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    # yazdır(results.multi_hand_landmarks)
+    result = hands.process(imgRGB)
  
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                # yazdır(id, lm)
+              
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 print(id, cx, cy)
-                # eger eşitse id == 4:
+             
                 cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
  
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
